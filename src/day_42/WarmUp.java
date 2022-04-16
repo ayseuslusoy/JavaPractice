98c024706fc86b5ad27757c2e905df3e0ff292a2
Ayse Uslusoy <97849839+ayseuslusoy@users.noreply.github.com>
2022-04-16 11:00:36 +0100

    Exceptions

---
+++ src/day_42/WarmUp.java
@@ -0,0 +1,25 @@
+package day_42;
+
+public class WarmUp {
+    public static void main(String[] args) {
+        System.out.println("---------push up started---------");
+         for(int i=1; i<=10; i++) {
+             System.out.print(" Push up " + i);
+
+
+             try {
+                 Thread.sleep(1500);
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+
+         }
+
+
+
+        System.out.println("\n--------push up finished-----------");
+
+        //Do 30 push-ups and try pausing 1.5 seconds in each
+
+    }
+}
